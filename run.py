4ec9bd8fcb62fa86355eaa15aafb04c7b61adf65
Wayne Halloran <whalloranirl@protonmail.com>
2024-03-14 12:02:52 +0000

    Added function to update sales worksheet

--- run.py
+++ run.py
@@ -30,8 +30,10 @@ def get_sales_data():
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
-            print("Data is valid")
+            print("Data is valid\n")
             break
+
+    return sales_data
 
 def validate_data(values):
     try:
@@ -46,4 +48,16 @@ def validate_data(values):
 
     return True
 
+
+def update_sales_data(data):
+    """
+    Update the sales worksheet, add a new row with the data values
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales sheet updated \n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_data(sales_data)
